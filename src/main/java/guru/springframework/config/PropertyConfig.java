a22af3fc8d8d675097e1ed044fda5cdba758af19
twana <twja@chess-ix.com>
2018-06-05 20:57:47 +0200

    override property value with system environment

--- src/main/java/guru/springframework/config/PropertyConfig.java
+++ src/main/java/guru/springframework/config/PropertyConfig.java
@@ -1,14 +1,19 @@
 package guru.springframework.config;
 
 import guru.springframework.examplebeans.FakeDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 
 @Configuration
 @PropertySource("classpath:datasource.properties")
 public class PropertyConfig {
+    @Autowired
+    private Environment environment;
+
     @Value("${guru.username}")
     private String userName;
     @Value("${guru.password}")
@@ -19,7 +24,18 @@ public class PropertyConfig {
     @Bean
     public FakeDataSource fakeDataSource() {
         FakeDataSource fakeDataSource = new FakeDataSource();
-        fakeDataSource.setUsername(userName);
+        // fakeDataSource.setUsername(userName);
+        /*
+        I have created a system environment property with the name username
+        Go to the run configuration "DiDemoApplication"
+        Under the Environment -> Environment variable add a new variable an give it a value
+
+        Or
+        you cane just override the guru.username in the Environment variable
+        by adding GURU_USERNAME (low case works too but with dot didnot work) and give it a value
+         */
+        fakeDataSource.setUsername(environment.getProperty("username"));
+
         fakeDataSource.setPassword(password);
         fakeDataSource.setUrl(dbUrl);
 
