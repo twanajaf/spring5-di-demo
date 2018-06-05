216199a855804752ad6f3ca29ba82cd1875e9f55
twana <twja@chess-ix.com>
2018-06-05 21:22:57 +0200

    cleaning

--- src/main/java/guru/springframework/config/PropertyConfig.java
+++ src/main/java/guru/springframework/config/PropertyConfig.java
@@ -2,12 +2,10 @@ package guru.springframework.config;
 
 import guru.springframework.examplebeans.FakeDataSource;
 import guru.springframework.examplebeans.FakeJmsBroker;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
 
 @Configuration
 @PropertySource({"classpath:datasource.properties", "classpath:jms.properties"})
@@ -20,8 +18,6 @@ import org.springframework.core.env.Environment;
                  })
                   */
 public class PropertyConfig {
-    @Autowired
-    private Environment environment;
 
     @Value("${guru.username}")
     private String userName;
